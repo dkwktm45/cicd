1edff851ccaf02dc2f8b4c8bee12c38a2026e15f
dkwktm45 <dkwktm45@naver.com>
2023-11-30 17:25:16 +0900

    spring만을 test하기 위한 의존성 제거[Database 부분 제거]

--- src/main/java/com/demo/cicd/controller/Controller.java
+++ src/main/java/com/demo/cicd/controller/Controller.java
@@ -1,31 +1,13 @@
 package com.demo.cicd.controller;
 
-import com.demo.cicd.domain.Member;
-import com.demo.cicd.domain.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class Controller {
-
-  private final MemberRepository memberRepository;
 
   @PostMapping("/health-check")
   public String health(){
     return "health-server";
   }
-
-  @PostMapping("/health-check")
-  public Member login(){
-    return memberRepository.save(Member.builder()
-        .sex("남성")
-        .email("test@gmail.com")
-        .password("testPassword")
-        .nickName("testUser")
-        .build()
-    );
-  }
-
 }
--- src/main/java/com/demo/cicd/domain/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.cicd.domain;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-
-}
