db98370a0e23f025e3db044ec8942fa349d6a7ee
dkwktm45 <dkwktm45@naver.com>
2023-12-01 14:12:38 +0900

    webhook test

--- src/main/java/com/demo/cicd/controller/Controller.java
+++ src/main/java/com/demo/cicd/controller/Controller.java
@@ -1,12 +1,12 @@
 package com.demo.cicd.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-  @PostMapping("/health-check")
+  @GetMapping("/health-check")
   public String health(){
     return "health-server";
   }
