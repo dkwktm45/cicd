b32bbf0266e481273c36b65a8e05f716a6592213
dkwktm45 <dkwktm45@naver.com>
2023-12-02 19:38:55 +0900

    logback 설정

--- src/main/java/com/demo/cicd/controller/Controller.java
+++ src/main/java/com/demo/cicd/controller/Controller.java
@@ -1,13 +1,20 @@
 package com.demo.cicd.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Controller {
 
   @GetMapping("/health-check")
   public String health(){
+    log.info("teste");
+    log.warn("teste");
+    log.debug("teste");
+    log.info("teste");
+
     return "health-server";
   }
 }
